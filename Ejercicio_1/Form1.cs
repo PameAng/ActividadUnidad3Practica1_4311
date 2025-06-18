0bbacde7479644cb9a41031d92f0ad9a4cf0f3d9
PameAng <pamelaangomas@hotmail.com>
2025-06-18 16:43:37 -0300

    Primer ejercicio

---
+++ Ejercicio_1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtnumero.Text))
+                    {
+                MessageBox.Show("Debe introducir un número","Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int numero = int.Parse(txtnumero.Text);
+
+            for (int i = 1; i <= 12; i++)
+            {
+                lbtabla.Items.Add($"{numero}*{i}={i * numero}");
+            }
+        }
+    }
+}
