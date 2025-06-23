156831a5abde75d09357897a8c504c809502e211
PameAng <pamelaangomas@hotmail.com>
2025-06-23 15:58:25 -0300

    Modificacion ejercicio 5

--- Ejercicio_5/Form1.Designer.cs
+++ Ejercicio_5/Form1.Designer.cs
@@ -30,34 +30,52 @@
         {
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.btnVerificar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(420, 89);
+            this.maskedTextBox1.BackColor = System.Drawing.Color.Moccasin;
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(56, 98);
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 22);
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 30);
             this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btnVerificar
             // 
-            this.btnVerificar.Location = new System.Drawing.Point(434, 143);
+            this.btnVerificar.BackColor = System.Drawing.Color.Moccasin;
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(346, 163);
             this.btnVerificar.Name = "btnVerificar";
-            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.Size = new System.Drawing.Size(137, 102);
             this.btnVerificar.TabIndex = 1;
             this.btnVerificar.Text = "Verificar";
-            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.UseVisualStyleBackColor = false;
             this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese un número";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(495, 277);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnVerificar);
             this.Controls.Add(this.maskedTextBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Números primos";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +85,7 @@
 
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label label1;
     }
 }
 
