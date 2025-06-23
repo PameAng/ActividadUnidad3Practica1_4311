e7ac3ccb966870cb85ea44e25111949a3cd7dc3b
PameAng <pamelaangomas@hotmail.com>
2025-06-23 15:31:32 -0300

    modificaciones ejericio 2

--- Ejercicio_2/Form1.Designer.cs
+++ Ejercicio_2/Form1.Designer.cs
@@ -45,60 +45,74 @@
             // 
             // txtmin
             // 
-            this.txtmin.Location = new System.Drawing.Point(46, 40);
+            this.txtmin.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.txtmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmin.Location = new System.Drawing.Point(41, 66);
+            this.txtmin.Multiline = true;
             this.txtmin.Name = "txtmin";
-            this.txtmin.Size = new System.Drawing.Size(100, 22);
+            this.txtmin.Size = new System.Drawing.Size(100, 30);
             this.txtmin.TabIndex = 0;
+            this.txtmin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtseg
             // 
-            this.txtseg.Location = new System.Drawing.Point(177, 40);
+            this.txtseg.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.txtseg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtseg.Location = new System.Drawing.Point(181, 66);
+            this.txtseg.Multiline = true;
             this.txtseg.Name = "txtseg";
-            this.txtseg.Size = new System.Drawing.Size(100, 22);
+            this.txtseg.Size = new System.Drawing.Size(111, 30);
             this.txtseg.TabIndex = 1;
+            this.txtseg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btniniciar
             // 
-            this.btniniciar.Location = new System.Drawing.Point(63, 115);
+            this.btniniciar.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btniniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btniniciar.Location = new System.Drawing.Point(96, 196);
             this.btniniciar.Name = "btniniciar";
-            this.btniniciar.Size = new System.Drawing.Size(75, 23);
+            this.btniniciar.Size = new System.Drawing.Size(132, 49);
             this.btniniciar.TabIndex = 2;
             this.btniniciar.Text = "Iniciar";
-            this.btniniciar.UseVisualStyleBackColor = true;
+            this.btniniciar.UseVisualStyleBackColor = false;
             this.btniniciar.Click += new System.EventHandler(this.btniniciar_Click);
             // 
             // lbltiempo
             // 
             this.lbltiempo.AutoSize = true;
-            this.lbltiempo.Location = new System.Drawing.Point(201, 118);
+            this.lbltiempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltiempo.Location = new System.Drawing.Point(130, 140);
             this.lbltiempo.Name = "lbltiempo";
-            this.lbltiempo.Size = new System.Drawing.Size(38, 16);
+            this.lbltiempo.Size = new System.Drawing.Size(67, 25);
             this.lbltiempo.TabIndex = 3;
             this.lbltiempo.Text = "00:00";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.Size = new System.Drawing.Size(81, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "Minutos";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(201, 9);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(189, 39);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.Size = new System.Drawing.Size(102, 25);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Segundo";
+            this.label2.Text = "Segundos";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(350, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbltiempo);
@@ -106,7 +120,7 @@
             this.Controls.Add(this.txtseg);
             this.Controls.Add(this.txtmin);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Temporizador";
             this.ResumeLayout(false);
             this.PerformLayout();
 
