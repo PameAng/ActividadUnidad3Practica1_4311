51ffecd3347d363b909828b20755a573eed9cc5f
PameAng <pamelaangomas@hotmail.com>
2025-06-23 15:46:28 -0300

    Modificación ejercicio 4

--- Ejercicio_4/Form1.Designer.cs
+++ Ejercicio_4/Form1.Designer.cs
@@ -36,34 +36,42 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(307, 50);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 93);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 16);
+            this.label1.Size = new System.Drawing.Size(222, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Introduce una palabra";
             // 
             // btnVerificar
             // 
-            this.btnVerificar.Location = new System.Drawing.Point(298, 129);
+            this.btnVerificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(180, 348);
             this.btnVerificar.Name = "btnVerificar";
-            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.Size = new System.Drawing.Size(126, 56);
             this.btnVerificar.TabIndex = 1;
-            this.btnVerificar.Text = "button1";
-            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = false;
             this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
             // 
             // txtPalabra
             // 
-            this.txtPalabra.Location = new System.Drawing.Point(298, 91);
+            this.txtPalabra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtPalabra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPalabra.Location = new System.Drawing.Point(129, 162);
+            this.txtPalabra.Multiline = true;
             this.txtPalabra.Name = "txtPalabra";
-            this.txtPalabra.Size = new System.Drawing.Size(100, 22);
+            this.txtPalabra.Size = new System.Drawing.Size(231, 44);
             this.txtPalabra.TabIndex = 2;
+            this.txtPalabra.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(575, 450);
             this.Controls.Add(this.txtPalabra);
             this.Controls.Add(this.btnVerificar);
             this.Controls.Add(this.label1);
